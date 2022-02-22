6dc4eabda20cd91160ca24221ad18f463adcf79c
Jason Karns <jason.karns@gmail.com>
2022-02-22 09:52:43 -0500

    non-AR repro

--- spec/mailers/repro_spec.rb
+++ spec/mailers/repro_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
 
+class M
+  include GlobalID::Identification
+
+  def id
+    1
+  end
+end
 RSpec.describe ReproMailer, type: :mailer do
   it "should repro" do
     expect {
-      ReproMailer.with(inquiry: Inquiry.new(id: 1)).message.deliver_later
+      ReproMailer.with(inquiry: M.new).message.deliver_later
     }.to have_enqueued_email(ReproMailer, :message)
   end
 end
